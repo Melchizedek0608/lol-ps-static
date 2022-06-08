24d1653760634bf51e3a1d94add5e1fe66d54b15
toBeSD <kuzoteki@gmail.com>
2022-06-08 09:44:30 +0900

    happy birthday

--- Js/all.js
+++ Js/all.js
@@ -1,7 +1,6 @@
 const navbar = document.querySelector('.header-mainnav');
 
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY)
     if(window.scrollY > 160) {
         navbar.style.background = '#172b65';
     }else if(window.scrollY < 160) {
