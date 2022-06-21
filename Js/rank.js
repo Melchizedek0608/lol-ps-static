e502dd393e6c817317c13f418675d55002c1e5a6
toBeSD <kuzoteki@gmail.com>
2022-06-21 12:14:41 +0900

    rank 정리

--- Js/rank.js
+++ Js/rank.js
@@ -6,34 +6,7 @@ const adBtn = document.querySelectorAll('.rank-position-button')[3];
 const supBtn = document.querySelectorAll('.rank-position-button')[4];
 const allBtn = document.querySelectorAll('.rank-position-button')[5];
 
-$.ajax({
-    type: "POST",
-    url: '/champ/rankline',
-    data: JSON.stringify({ "line" : "탑" }),
-    dataType: 'JSON',
-    contentType : 'application/json',
-    success: function (data) {
-        let list = '';
-        for(let i = 0; i < data.length; i++) {
-            list +=`
-               <div class="list">
-                    <span>${i + 1}</span>
-                    <div style="width: 40px; padding: 0;">
-                        <img class="list-image" src="../image/champhead/${data[i].image}" alt="img">
-                    </div>
-                    <span style="width: 30%;">${data[i].name}</span>
-                    <span>${data[i].line}</span>
-                    <span class="list-number">${data[i].psScore}</span>
-                    <span class="list-number">${data[i].honeyScore}</span>
-                    <span class="list-number">${data[i].winRate}</span>
-                    <span class="list-number">${data[i].pickRate}</span>
-                    <span class="list-number">${data[i].banRate}</span>
-                    <span class="list-number">${data[i].count}</span>
-                </div>`;
-        }
-        $('.champ-list').append(list);
-    },
-});
+
 
 position.addEventListener('click', (e) => {
     if(e.target === topBtn) {
@@ -46,39 +19,8 @@ position.addEventListener('click', (e) => {
 
         topBtn.classList.add('active');
 
-        $.ajax({
-            type: "POST",
-            url: '/champ/rankline',
-            data: JSON.stringify({ "line" : "탑" }),
-            dataType: 'JSON',
-            contentType : 'application/json',
-            success: function (data) {
-                $('.champ-list').remove();
-                let champList = $("<div>").attr('class', 'champ-list');
-                let list = '';
-                for(let i = 0; i < data.length; i++) {
-                    list +=`
-                       <div class="list">
-                            <span>${i + 1}</span>
-                            <div style="width: 40px; padding: 0;">
-                                <img class="list-image" src="../image/champhead/${data[i].image}" alt="img">
-                            </div>
-                            <span style="width: 30%;">${data[i].name}</span>
-                            <span>${data[i].line}</span>
-                            <span class="list-number">${data[i].psScore}</span>
-                            <span class="list-number">${data[i].honeyScore}</span>
-                            <span class="list-number">${data[i].winRate}</span>
-                            <span class="list-number">${data[i].pickRate}</span>
-                            <span class="list-number">${data[i].banRate}</span>
-                            <span class="list-number">${data[i].count}</span>
-                        </div>`;
-                }
-                champList.append(list);
-                $('.champ-rank-container').append(champList);
-            },
-        });
-
     } 
+
     if(e.target === jungleBtn) {
         topBtn.classList.remove('active');
         jungleBtn.classList.remove('active');
@@ -99,39 +41,8 @@ position.addEventListener('click', (e) => {
         allBtn.classList.remove('active');
 
         midBtn.classList.add('active');
+    }
 
-        $.ajax({
-            type: "POST",
-            url: '/champ/rankline',
-            data: JSON.stringify({ "line" : "미드" }),
-            dataType: 'JSON',
-            contentType : 'application/json',
-            success: function (data) {
-                $('.champ-list').remove();
-                let champList = $("<div>").attr('class', 'champ-list');
-                let list = '';
-                for(let i = 0; i < data.length; i++) {
-                    list +=`
-                       <div class="list">
-                            <span>${i + 1}</span>
-                            <div style="width: 40px; padding: 0;">
-                                <img class="list-image" src="../image/champhead/${data[i].image}" alt="img">
-                            </div>
-                            <span style="width: 30%;">${data[i].name}</span>
-                            <span>${data[i].line}</span>
-                            <span class="list-number">${data[i].psScore}</span>
-                            <span class="list-number">${data[i].honeyScore}</span>
-                            <span class="list-number">${data[i].winRate}</span>
-                            <span class="list-number">${data[i].pickRate}</span>
-                            <span class="list-number">${data[i].banRate}</span>
-                            <span class="list-number">${data[i].count}</span>
-                        </div>`;
-                }
-                champList.append(list);
-                $('.champ-rank-container').append(champList);
-            },
-        });
-    } 
     if(e.target === adBtn) {
 
         topBtn.classList.remove('active');
@@ -142,39 +53,8 @@ position.addEventListener('click', (e) => {
         allBtn.classList.remove('active');
 
         adBtn.classList.add('active');
-
-        $.ajax({
-            type: "POST",
-            url: '/champ/rankline',
-            data: JSON.stringify({ "line" : "원딜" }),
-            dataType: 'JSON',
-            contentType : 'application/json',
-            success: function (data) {
-                $('.champ-list').remove();
-                let champList = $("<div>").attr('class', 'champ-list');
-                let list = '';
-                for(let i = 0; i < data.length; i++) {
-                    list +=`
-                       <div class="list">
-                            <span>${i + 1}</span>
-                            <div style="width: 40px; padding: 0;">
-                                <img class="list-image" src="../image/champhead/${data[i].image}" alt="img">
-                            </div>
-                            <span style="width: 30%;">${data[i].name}</span>
-                            <span>${data[i].line}</span>
-                            <span class="list-number">${data[i].psScore}</span>
-                            <span class="list-number">${data[i].honeyScore}</span>
-                            <span class="list-number">${data[i].winRate}</span>
-                            <span class="list-number">${data[i].pickRate}</span>
-                            <span class="list-number">${data[i].banRate}</span>
-                            <span class="list-number">${data[i].count}</span>
-                        </div>`;
-                }
-                champList.append(list);
-                $('.champ-rank-container').append(champList);
-            },
-        });
     } 
+
     if(e.target === supBtn) {
         topBtn.classList.remove('active');
         jungleBtn.classList.remove('active');
@@ -184,38 +64,6 @@ position.addEventListener('click', (e) => {
         allBtn.classList.remove('active');
 
         supBtn.classList.add('active');
-
-        $.ajax({
-            type: "POST",
-            url: '/champ/rankline',
-            data: JSON.stringify({ "line" : "서폿" }),
-            dataType: 'JSON',
-            contentType : 'application/json',
-            success: function (data) {
-                $('.champ-list').remove();
-                let champList = $("<div>").attr('class', 'champ-list');
-                let list = '';
-                for(let i = 0; i < data.length; i++) {
-                    list +=`
-                       <div class="list">
-                            <span>${i + 1}</span>
-                            <div style="width: 40px; padding: 0;">
-                                <img class="list-image" src="../image/champhead/${data[i].image}" alt="img">
-                            </div>
-                            <span style="width: 30%;">${data[i].name}</span>
-                            <span>${data[i].line}</span>
-                            <span class="list-number">${data[i].psScore}</span>
-                            <span class="list-number">${data[i].honeyScore}</span>
-                            <span class="list-number">${data[i].winRate}</span>
-                            <span class="list-number">${data[i].pickRate}</span>
-                            <span class="list-number">${data[i].banRate}</span>
-                            <span class="list-number">${data[i].count}</span>
-                        </div>`;
-                }
-                champList.append(list);
-                $('.champ-rank-container').append(champList);
-            },
-        });
     }
 
     if(e.target === allBtn) {
@@ -228,38 +76,5 @@ position.addEventListener('click', (e) => {
 
         allBtn.classList.add('active');
 
-        $.get('/champ/rankall')
-            .done((data) => {
-                $('.champ-list').remove();
-                let champList = $("<div>").attr('class', 'champ-list');
-                let list = '';
-                for(let i = 0; i < data.length; i++) {
-                     list +=`
-                     <a href="/champ/statistics?name=${data[i].name}" style="text-decoration: none">
-                       <div class="list">
-                            <span>${i + 1}</span>
-                            <div style="width: 40px; padding: 0;">
-                                <img class="list-image" src="../image/champhead/${data[i].image}" alt="img">
-                            </div>
-                            <span style="width: 30%;">${data[i].name}</span>
-                            <span>${data[i].line}</span>
-                            <span class="list-number">${data[i].psScore}</span>
-                            <span class="list-number">${data[i].honeyScore}</span>
-                            <span class="list-number">${data[i].winRate}</span>
-                            <span class="list-number">${data[i].pickRate}</span>
-                            <span class="list-number">${data[i].banRate}</span>
-                            <span class="list-number">${data[i].count}</span>
-                        </div>
-                     </a>`;
-                }
-                champList.append(list);
-                $('.champ-rank-container').append(champList);
-            })
     }
-})
-
-const goTop = document.querySelector('.top-button');
-
-goTop.addEventListener('click', () => {
-    window.scrollTo({ top:0, behavior: 'smooth' })
 })
